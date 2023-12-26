b098cb85cf6dfad9ae798d76deef380e381feafe
Kübra Tanrıkulu <ktanrikuluu@gmail.com>
2023-12-27 01:24:20 +0300

    feat(server): install cors packet and edit appjs

--- server/app.js
+++ server/app.js
@@ -1,19 +1,28 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const connection = require('./config/connection')
-const BookRouter = require('./routes/BookRouter')
-dotenv.config()
-connection()
-const app = express();
-const port = process.env.PORT || 3001
+const express = require('express');
+const dotenv = require('dotenv');
+const connection = require('./config/connection');
+const BookRouter = require('./routes/BookRouter');
+const cors = require('cors');
 
-app.use(express.json())
-app.use('/books', BookRouter)
+dotenv.config();
+connection();
+
+const app = express();
+
+app.use(cors({
+  origin: "*"
+}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/books', BookRouter);
+
 app.get('/', (req, res) => {
-    res.send('Merhaba, harun!');
-  });
-  
-  app.listen(port, () => {
-    console.log(`Uygulama http://localhost:${port} adresinde çalışıyor`);
-  });
-  
+  res.send('Merhaba, harun!');
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Uygulama http://localhost:${port} adresinde çalışıyor`);
+});
