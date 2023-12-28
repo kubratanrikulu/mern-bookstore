faad0697d1df6622276dafa2d1c9f70a6cbde1a7
Kübra Tanrıkulu <ktanrikuluu@gmail.com>
2023-12-28 23:07:55 +0300

    feat(ui): add banner section on homepage

---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import banner from '../assets/images/banner.jpeg'
+
+const Banner = () => {
+    return (
+        <>
+            <img src={banner} alt="" />
+        </>
+    )
+}
+
+export default Banner
