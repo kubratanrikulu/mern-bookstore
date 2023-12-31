21fe0c423923266d1301758c3ab4e216910d2054
Kübra Tanrıkulu <ktanrikuluu@gmail.com>
2024-01-01 01:05:19 +0300

    style(ui): add roots values(font-family-color)

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ebGaramond: ['"EB Garamond"', "sans-serif"],
+        raleway: ['"Raleway"', "sans-serif"],
+      },
+      colors: {
+        primary: "#f86d72",
+        h1Color: "#2b2b2b",
+        pColor: "#909090",
+        footerText: "#666"
+      },
+    },
   },
   plugins: [],
 }
